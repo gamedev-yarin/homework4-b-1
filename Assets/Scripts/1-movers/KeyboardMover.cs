46c8d4dfd7ceaf0f1415182d2b98e312525309a5
yarin Shapira <yariniii19@gmail.com>
2020-11-17 16:04:19 +0200

    Initial commit

---
+++ Assets/Scripts/1-movers/KeyboardMover.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component moves its object when the player clicks the arrow keys.
+ */
+public class KeyboardMover: MonoBehaviour {
+    [Tooltip("Speed of movement, in meters per second")]
+    [SerializeField] float speed = 1f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
+        float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
+        Vector2 movementVector = new Vector2(horizontal, vertical) * speed;
+        rb.velocity = movementVector;
+        //transform.Translate(movementVector);
+        // NOTE: "Translate(movementVector)" uses relative coordinates - 
+        //       it moves the object in the coordinate system of the object itself.
+        // In contrast, "transform.position += movementVector" would use absolute coordinates -
+        //       it moves the object in the coordinate system of the world.
+        // It makes a difference only if the object is rotated.
+    }
+}
